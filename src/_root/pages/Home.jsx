89e0355860c84a704c38f8436fd4de9c76ce5385
fh-rabbi <frabby983@gmail.com>
2024-03-05 17:56:22 +0600

    Added important notice in home page

--- src/_root/pages/Home.jsx
+++ src/_root/pages/Home.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react"
 import { Hero, FeatureCard } from "@/components";
 import { features } from "@/constants";
 import { Helmet } from "react-helmet-async";
 
 export const Home = () => {
+  useEffect(() => {
+    confirm(`📣 NOTICE: Sign-Up Currently Unavailable!
+      \nI'm sorry, but sign-up functionality is temporarily unavailable at the moment. My Link-Bridge-Api service has been suspended due to exceeding the monthly request quota. I'm actively working on resolving this issue.
+    `)
+  },[]);
+    
   return (
     <>
       <Helmet>
@@ -10,7 +17,7 @@ export const Home = () => {
       </Helmet>
       
       <Hero />
-
+      
       {/* Features */}
       <section className="">
         <ul className="mt-10 md:mt-14 grid grid-cols-1 md:grid-cols-2 gap-4">

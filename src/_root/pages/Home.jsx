eda81a63719130d74d16880a7b4d517b3b4cc046
Fazle Rabbi <frabby983@gmail.com>
2024-08-31 14:21:55 +0600

    Remove app overview video from home page

--- src/_root/pages/Home.jsx
+++ src/_root/pages/Home.jsx
@@ -13,8 +13,6 @@ export const Home = () => {
       
       <Hero />
       
-      <OverviewVideo />
-      
       {/* Features */}
       <section className="">
         <ul className="mt-10 md:mt-14 grid grid-cols-1 md:grid-cols-2 gap-4">
